fbd6f7204dbd096d6d81d09f45c58f0273488391
levanhieusona <levanhieusona@gmail.com>
2023-02-17 10:23:55 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+window.addEventListener("load", function () {
+    let d = new Date();
+    let ngay = [
+        "Chủ nhật",
+        "Thứ hai",
+        "Thứ ba",
+        "Thứ tư",
+        "Thứ năm",
+        "Thứ sáu",
+        "Thứ bảy",
+    ];
+    // let thang = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+    const descTime = document.querySelector(".desc-data-time");
+    let month = d.getMinutes();
+    if (month < 10) {
+        month = "0" + month;
+    }
+    descTime.textContent =
+        `Hôm nay, ${ngay[d.getDay()]} ngày ${d.getDate()} tháng ${
+            d.getMonth() + 1
+        } năm ${d.getFullYear()}, ${d.getHours()}:` + month;
+    const imgPanel = document.querySelector(".img-panel");
+    console.log(imgPanel);
+    let vt = 0;
+    imgPanel.addEventListener("click", handlePanel);
+    function handlePanel() {
+        const imgPanels = [
+            "./assets/css/img/st1.jpg",
+            "./assets/css/img/st2.jpg",
+            "./assets/css/img/st3.jpg",
+            "./assets/css/img/st4.jpg",
+            "./assets/css/img/st5.jpg",
+            "./assets/css/img/st6.jpg",
+            "./assets/css/img/st7.jpg",
+            "./assets/css/img/st8.jpg",
+        ];
+        imgPanel.src = imgPanels[vt];
+        vt++;
+        if (vt > 7) {
+            vt = 0;
+        }
+    }
+    setInterval(handlePanel, 2500);
+});
---
+++ trangchu.js
@@ -0,0 +1,52 @@
+window.addEventListener("load", function () {
+    let mainBanner = document.querySelector(".main-banner");
+    mainBanner.addEventListener("click", handleImgaes);
+    let index = 0;
+    let imgs = [
+        "./assets/css/img/tuyensinhlop10js.jpg",
+        "./assets/css/img/lophocjs.jpg",
+        "./assets/css/img/thethaojs.jpg",
+        "./assets/css/img/thithptjs.jpg",
+        "./assets/css/img/hoaphuongjs.jpg",
+        "./assets/css/img/anhbantin1.jpg",
+    ];
+    let content = [
+        "TUYỂN SINH LỚP 10 NĂM HỌC 2022-2023",
+        "HỘI THI GIÁO VIÊN DẠY GIỎI CẤP TỈNH NĂM HỌC...",
+        "HOẠT ĐỘNG THỂ DỤC THỂ THAO",
+        "CHIẾN DỊCH TÌNH NGUYỆN “TIẾP SỨC MÙA THI” NĂM 2022",
+        'CHIẾN DỊCH TÌNH NGUYỆN "HOA PHƯỢNG ĐỎ" NĂM 2022',
+        "GIẢI BÓNG ĐÁ MINI NAM NỮ TRUYỀN THỐNG TRƯỜNG THPT...",
+    ];
+    const thumbs = document.querySelectorAll(".head .block-right .thumb");
+    function handleImgaes(e) {
+        mainBanner.src = imgs[index];
+        mainBanner.nextElementSibling.textContent = content[index];
+        index++;
+        if (index === 6) {
+            index = 0;
+        }
+    }
+    const rightJs = document.querySelector(".right-js");
+    rightJs.addEventListener("click", handleNextImg);
+
+    function handleNextImg() {
+        index++;
+        if (index === 6) {
+            index = 0;
+        }
+        mainBanner.src = imgs[index];
+        mainBanner.nextElementSibling.textContent = content[index];
+    }
+
+    const leftJs = document.querySelector(".left-js");
+    leftJs.addEventListener("click", function (e) {
+        index--;
+        if (index < 0) {
+            index = 5;
+        }
+        mainBanner.src = imgs[index];
+        mainBanner.nextElementSibling.textContent = content[index];
+    });
+    setInterval(handleImgaes, 4000);
+});
